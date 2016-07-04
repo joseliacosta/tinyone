2d60e692977f75a7aec6f138704b1c0b8c965aa2
Josélia Costa <joselialcosta@gmail.com>
2016-07-04 00:44:50 -0300

    Added Ajax to form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,29 @@
-var contactform =  document.getElementById('contactform');
-    contactform.setAttribute('action', '//formspree.io/' + 'joselialcosta' + '@' + 'gmail' + '.' + 'com');
 
+
+$(document).ready(function() {
+  $('#contactForm').submit(function(e) {
+      var name = $('#name')
+      var email = $('#email')
+      var phone = $('#phone')
+    
+      if(name.val() == "" || email.val() == "" || phone.val() == "") {
+        $('.submit-fail').fadeToggle(400);
+        return false;
+      }
+      else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/joselialcosta@gmail.com',
+          data: $('#contactForm').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        $('.submit-success').fadeToggle(400);
+      }
+    });
+  
+  $('.submit-fail, .submit-success').click(function() {
+    $(this).hide();
+  })
+});
