d3a122ca111a808e2c135c3cab6cadbdce0fb9ff
Josélia Costa <joselialcosta@gmail.com>
2016-07-04 01:14:54 -0300

    Test with ajax

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $('#contact-form').submit(function(e) {
       var name = $('#inputName')
       var email = $('#inputEmail')
-      var message = $('#inputMessage')
+      var phone = $('#inputPhone')
     
-      if(name.val() == "" || email.val() == "" || message.val() == "") {
+      if(name.val() == "" || email.val() == "" || phone.val() == "") {
         $('.submit-fail').fadeToggle(400);
         return false;
       }
@@ -14,10 +14,10 @@ $(document).ready(function() {
           url: '//formspree.io/joselialcosta@gmail.com',
           data: $('#contact-form').serialize(),
           datatype: 'json'
-        });
+        });*/
         e.preventDefault();
         $(this).get(0).reset();
-        $('.submit-success').fadeToggle(400);*/
+        $('.submit-success').fadeToggle(400);
         console.log("Work!")
       }
     });
